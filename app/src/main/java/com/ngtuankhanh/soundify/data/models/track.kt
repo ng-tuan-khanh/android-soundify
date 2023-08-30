f024b6cca0294cadbba758527e187765954787d1
LeViet <leviettt.tnh@gmail.com>
2023-08-30 21:08:13 +0700

    feat: create Track model

---
+++ app/src/main/java/com/ngtuankhanh/soundify/data/models/track.kt
@@ -0,0 +1,8 @@
+package com.ngtuankhanh.soundify.data.models
+
+class Track {
+    var ID: String? = null
+    var Album_ID: String? = null
+    var Artist_ID: String? = null
+    var URL: String? = null
+}
