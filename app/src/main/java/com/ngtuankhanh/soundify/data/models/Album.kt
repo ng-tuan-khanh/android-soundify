67a4480a0f2ebfedfaacda79e089a54def74d14b
LeViet <leviettt.tnh@gmail.com>
2023-08-30 21:42:22 +0700

    feat: create Album model

---
+++ app/src/main/java/com/ngtuankhanh/soundify/data/models/Album.kt
@@ -0,0 +1,11 @@
+package com.ngtuankhanh.soundify.data.models
+
+class Album {
+    var ID: String? = null
+    var Name: String? = null
+    var ArtistID: String? = null
+    var ArtistName: String? = null
+    var ImageURL: String? = null
+    var ReleaseDate: String? = null
+    var ListOfTracks: Array<Track> = arrayOf()
+}
