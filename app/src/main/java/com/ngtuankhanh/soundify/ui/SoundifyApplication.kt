648ea389966d88e94558e04715065f269ccfb776
ng-tuan-khanh <ntklxag@gmail.com>
2023-09-04 15:48:51 +0700

    feat: implement the FeaturedPlaylistsRepository

--- app/src/main/java/com/ngtuankhanh/soundify/ui/SoundifyApplication.kt
+++ app/src/main/java/com/ngtuankhanh/soundify/ui/SoundifyApplication.kt
@@ -1,15 +1,28 @@
 package com.ngtuankhanh.soundify.ui
 
 import android.app.Application
+import android.content.Context
 import com.adamratzman.spotify.auth.SpotifyDefaultCredentialStore
 import com.ngtuankhanh.soundify.BuildConfig
 
-class SoundifyApplication: Application() {
+class SoundifyApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        context = applicationContext
+    }
+
+    companion object {
+        lateinit var context: Context
+    }
+}
+
+object SpotifyApi {
     val credentialStore by lazy {
         SpotifyDefaultCredentialStore(
             clientId = BuildConfig.SPOTIFY_CLIENT_ID,
             redirectUri = BuildConfig.SPOTIFY_REDIRECT_URI_PKCE,
-            applicationContext = applicationContext
+            applicationContext = SoundifyApplication.context
         )
     }
 }
