54dec0d1ad233ee6584add984588f2043e23b8ed
LeViet <leviettt.tnh@gmail.com>
2023-09-05 10:28:58 +0700

    feat: Create UI for Home screen

--- app/src/main/java/com/ngtuankhanh/soundify/ui/activities/MainActivity.kt
+++ app/src/main/java/com/ngtuankhanh/soundify/ui/activities/MainActivity.kt
@@ -9,16 +9,42 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.ngtuankhanh.soundify.R
 import com.ngtuankhanh.soundify.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        hideNavigationBar()
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        binding.bottomNavigationView.setupWithNavController(navController)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        hideNavigationBar()
+    }
+
+    private fun hideNavigationBar() {
+        val decorView = window.decorView
+        val uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                View.SYSTEM_UI_FLAG_FULLSCREEN or
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        decorView.systemUiVisibility = uiOptions
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
