3d34b023afa0ab459f7a80100ac0164ed631d574
LeViet <leviettt.tnh@gmail.com>
2023-09-06 15:17:18 +0700

    feat: add streamming bar

--- app/src/main/java/com/ngtuankhanh/soundify/ui/activities/MainActivity.kt
+++ app/src/main/java/com/ngtuankhanh/soundify/ui/activities/MainActivity.kt
@@ -17,12 +17,21 @@ import com.ngtuankhanh.soundify.R
 import com.ngtuankhanh.soundify.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.streamingButton.setOnClickListener {
+            val isSelected = it.isSelected
+            it.isSelected = !isSelected
+            // Thực hiện logic play/pause tại đây
+        }
+
 
         hideNavigationBar()
 
@@ -41,6 +50,16 @@ class MainActivity : AppCompatActivity() {
                 View.SYSTEM_UI_FLAG_FULLSCREEN or
                 View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
         decorView.systemUiVisibility = uiOptions
+    }
+
+    // Function to show the streaming bar
+    fun showStreamingBar() {
+        binding.streamingBar.visibility = View.VISIBLE
+    }
+
+    // Function to hide the streaming bar
+    fun hideStreamingBar() {
+        binding.streamingBar.visibility = View.GONE
     }
 
     override fun onSupportNavigateUp(): Boolean {
